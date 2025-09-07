1ee4b7279df287d6ddd8609a7e697cb5f9d900f6
bishal-j <bishal120101@gmail.com>
2025-09-07 10:52:59 +0530

    adds global error handler

--- campers/app.js
+++ campers/app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
+const { globalErrorHandler } = require("./controllers/errorController");
 
 const app = express();
 
@@ -19,5 +21,18 @@ app.use(express.static(`${__dirname}/public`));
 // 2. Routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+// Handling unhandled routes
+
+app.all("/*splat", (req, res, next) => {
+  // const err = new Error(`Can't find ${req.url} on this server!`);
+  // err.statusCode = 404;
+  // err.status = "fail";
+  // next(err);
+
+  next(new AppError(`Can't find ${req.url} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
