1d3dc7e9d1ea15e3368e449119293d81d3482c09
bishal-j <bishal120101@gmail.com>
2025-09-18 23:49:30 +0530

    imported new data

--- campers/dev-data/data/import-dev-data.js
+++ campers/dev-data/data/import-dev-data.js
@@ -15,9 +15,7 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 // READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
 const reviews = JSON.parse(
   fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
