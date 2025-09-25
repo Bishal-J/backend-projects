9b716f279efd76d8bd2e3d7e30ed43f3922510e9
bishal-j <bishal120101@gmail.com>
2025-09-25 08:28:08 +0530

    gettigs tours from a distance in mi or km

--- campers/controllers/tourController.js
+++ campers/controllers/tourController.js
@@ -275,6 +275,8 @@ exports.getDistances = catchAsync(async (req, res, next) => {
   const { latlng, unit } = req.params;
   const [lat, lng] = latlng.split(",");
 
+  const multiplier = unit === "mi" ? 0.000621371 : 0.001;
+
   if (!lat || !lng) {
     return next(
       new AppError(
@@ -292,7 +294,7 @@ exports.getDistances = catchAsync(async (req, res, next) => {
           coordinates: [lng * 1, lat * 1],
         },
         distanceField: "distance",
-        distanceMultiplier: 0.001,
+        distanceMultiplier: multiplier,
       },
     },
     {
